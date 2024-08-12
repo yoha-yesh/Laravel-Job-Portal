88fc485a45f566649aa3ee844164a81f7c51ed7c
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-12 18:14:28 +0300

    Use correct table name 'users' instead of 'user'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,7 +36,7 @@ class UserController extends Controller
     public function storeEmployer(Request $request){
         $request->validate([
             'name' => ['required','string', 'min:5'],
-            'email'=> ['required', 'email' ,Rule::unique('user')],
+            'email'=> ['required', 'email' ,Rule::unique('users')],
             'password' => ['required', 'confirmed' ,'min:4']
         ]);
 
@@ -49,13 +49,13 @@ class UserController extends Controller
 
         ]);
 
-       
+
 
         return redirect('/login')->with('success', 'Log in here');
 
 
 
-    } 
+    }
 
     public function logged(Request $request){
         $request->validate([
