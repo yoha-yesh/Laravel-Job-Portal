e15b662435d77b847809ee05007607cf664a8090
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-15 20:18:48 +0300

    Show real data in the individual listing page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,3 +1,6 @@
+@php
+    $tags = explode(',', $job->tags);
+@endphp
 @extends('layout.app')
 @section('content')
 <x-nav></x-nav>
@@ -16,32 +19,20 @@
                     alt=""
                 />
 
-                <h3 class="text-2xl mb-2">Senior Laravel Developer</h3>
-                <div class="text-xl font-bold mb-4">Acme Corp</div>
+                <h3 class="text-2xl mb-2">{{$job->title}}</h3>
+                <div class="text-xl font-bold mb-4">{{$job->company}}</div>
                 <ul class="flex">
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
-                        <a href="#">Laravel</a>
-                    </li>
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
-                        <a href="#">API</a>
-                    </li>
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
-                        <a href="#">Backend</a>
-                    </li>
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
-                        <a href="#">Vue</a>
-                    </li>
+                    @foreach ($tags as $tag)
+                        <li
+                            class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+                        >
+                            <a href="#">{{$tag}}</a>
+                        </li>
+                    @endforeach
                 </ul>
                 <div class="text-lg my-4">
-                    <i class="fa-solid fa-location-dot"></i> Daytona, FL
+                    <i class="fa-solid fa-location-dot"></i>
+                    {{$job->location}}
                 </div>
                 <div class="border border-gray-200 w-full mb-6"></div>
                 <div>
@@ -49,32 +40,17 @@
                         Job Description
                     </h3>
                     <div class="text-lg space-y-6">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur
-                            adipisicing elit. Eligendi non reprehenderit
-                            facilis architecto autem quam
-                            necessitatibus, odit quod, repellendus
-                            voluptate cum. Necessitatibus a id tenetur.
-                            Error numquam at modi quaerat.
-                        </p>
-                        <p>
-                            Lorem, ipsum dolor sit amet consectetur
-                            adipisicing elit. Quaerat praesentium eos
-                            consequuntur ex voluptatum necessitatibus
-                            odio quos cupiditate iste similique rem in,
-                            voluptates quod maxime animi veritatis illum
-                            quo sapiente.
-                        </p>
+                        {{$job->description}}
 
                         <a
-                            href="mailto:test@test.com"
+                            href="mailto:{{$job->email}}"
                             class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"
                             ><i class="fa-solid fa-envelope"></i>
                             Contact Employer</a
                         >
 
                         <a
-                            href="https://test.com"
+                            href="{{$job->website}}"
                             target="_blank"
                             class="block bg-black text-white py-2 rounded-xl hover:opacity-80"
                             ><i class="fa-solid fa-globe"></i> Visit
