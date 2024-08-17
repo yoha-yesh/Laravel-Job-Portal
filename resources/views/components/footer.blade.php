56852fbb2f6a65c7b8783a1465fc24b4259be5b1
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-17 19:01:23 +0300

    Show post jobs button only for employer role

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,16 +1,14 @@
 <footer
-            class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
-        >
-            <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
-            @auth
+    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
+>
+    <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+    @auth
+        @if(auth()->user()->role == 'employer')
             <a
                 href="{{route('create')}}"
                 class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
                 >Post Job</a
             >
-            @else
-            
-            @endauth
-
-           
-        </footer>
+        @endif
+    @endauth
+</footer>
